faea97da7c1a326d764c7a3e4e2f5741b9410eb5
Yun Xu <yunx@zillowgroup.com>
2018-03-26 20:05:56 -0700

    add TrieNode methods

--- trie/trie_node.go
+++ trie/trie_node.go
@@ -1,22 +1,44 @@
 package trie
 
-// Node trie tree node container carries value and children.
+import (
+	"path/filepath"
+)
+
+// Node trie tree node container carries name and children.
 type Node struct {
 	isLeaf bool
-	value  string
+	name  string
 	sep    string
-	child  map[string]*Node
+	children  map[string]*Node
 }
 
 
-// Insert - insert a child node.
-func (n *Node) Insert(child_node *Node) {
+// Get - get a child node.
+func (n *Node) Get(childName string) {
+	if child, ok := n.children[childName]; ok {
+		return child
+	} else {
+		return nil
+	}
+}
 
+// Insert - insert a child node.
+func (n *Node) Insert(childNode *Node) {
+	if child, ok := n.children[childNode.name]; ok {
+		if child.isLeaf 
+	} else {
+		n.children[childNode.name] = childNode
+	}
 }
 
 // Delete - delete a child_node.
-func (n *Node) Delete(child_name string) bool {
-
+func (n *Node) Delete(childName string) bool {
+	if _, ok := n.children[childName]; ok {
+		delete(n.children, childName)
+		return true
+	} else {
+		return false
+	}
 }
 
 // IsLeaf - check if node is leaf node.
@@ -26,5 +48,29 @@ func (n *Node) IsLeaf() bool {
 
 // GetAllNode - get all child nodes based on wild card query.
 func (n *Node) GetAllNode(pattern string) []*Node{
+	// TODO: Add expand braces logic
+	var matches []string
 
+	for childName, childNode := range n.children {
+		matched, err := Match(pattern, childName)
+		if err != nil {
+			// logging
+			continue
+		}
+
+		if matched {
+			matches.append(n.children[childName])
+		}
+	}
+	return matches
+}
+
+// ExpandQuery - expand a wildcard query.
+func (n *Node) ExpandQuery(query string) []string{
+
+}
+
+// Count - return children count
+func (n *Node) Count() int{
+	return len(n.children)
 }
